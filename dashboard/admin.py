9a9340f0723bbbf1cd3d60010b730afe3ab8889a
aapanco77 <alexiapanco@uci.edu.mx>
2022-06-28 12:49:22 -0500

    :rocket: Dashboard

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from .models import Team
-from .models import Project
 
 admin.site.register(Team)
-admin.site.register(Project)
--- dashboard/models.py
+++ dashboard/models.py
@@ -3,23 +3,17 @@ from django.db import models
 from django.utils import timezone
 
 class Team(models.Model):
-    coworker = models.CharField(max_length=30)
-    status = models.CharField(max_length=20)
-    assign_project = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.coworker
-
-class Project(models.Model):
-    """Proyectos de marca"""
-    company = models.CharField(max_length=20)
+    company = models.CharField(max_length=20, null=True)
     progress = models.SmallIntegerField(null=True, blank=True)
     death_line = models.DateTimeField(default=timezone.now)
     url_drive = models.URLField(null=True)
-    dummies = models.CharField(max_length=20)
-
+    dummies = models.CharField(max_length=20, null=True)
+    coworker = models.CharField(max_length=30)
+    status = models.CharField(max_length=20)
+    assign_project = models.CharField(max_length=20)
+    
     class Meta:
-        ordering = ['company', ]
+        ordering = ['company']
 
     def __str__(self):
         return self.company
